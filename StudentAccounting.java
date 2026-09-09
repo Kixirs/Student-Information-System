3047b7d81f8f17c25e34baba7667cc29d38e12b5
Kixirs <alina4403747@gmail.com>
2026-09-09 20:28:40 +0300

    Створено обчислювальну операцію

--- StudentAccounting.java
+++ StudentAccounting.java
@@ -18,3 +18,26 @@ void main()
 
     print("Чи студент отримує стипендію? (true/false): ");
     boolean hasScholarship = scanner.nextBoolean();
+
+    double deficit; //кількість балів, яких не вистачає студенту до наступного порогу
+    String scholarshipTier; //xназва цілі
+
+    if (averageGrade < 30) {
+        deficit = 30.0 - averageGrade;
+        scholarshipTier = "Мінімальна успішності";
+    } else if (averageGrade < 50) {
+        deficit = 50.0 - averageGrade;
+        scholarshipTier = "Задовільний рівень";
+    } else if (averageGrade < 70) {
+        deficit = 70.0 - averageGrade;
+        scholarshipTier = "Достатній рівень";
+    } else if (averageGrade < 85) {
+        deficit = 85.0 - averageGrade;
+        scholarshipTier = "Нарахування степендії";
+    } else if (averageGrade < 90) {
+        deficit = 90.0 - averageGrade;
+        scholarshipTier = "Нарахування підвищеної степендії";
+    } else {
+        deficit = 0.0;
+        scholarshipTier = "Максимальний рівень";
+    }
