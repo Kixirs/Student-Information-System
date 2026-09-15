3d31c1dd35a30a889e9fb8ce2655ff63ff95ce0d
Kixirs <alina4403747@gmail.com>
2026-09-15 23:51:32 +0300

    Створено клас 'Student'

---
+++ Student.java
@@ -0,0 +1,9 @@
+import java.util.Objects;
+
+public class Student {
+    public static final double MINIMAL_SUCCESS_THRESHOLD = 30.0;
+    public static final double SATISFACTORY_THRESHOLD = 50.0;
+    public static final double SUFFICIENT_THRESHOLD = 70.0;
+    public static final double SCHOLARSHIP_THRESHOLD = 85.0;
+    public static final double INCREASED_SCHOLARSHIP_THRESHOLD = 90.0;
+}
